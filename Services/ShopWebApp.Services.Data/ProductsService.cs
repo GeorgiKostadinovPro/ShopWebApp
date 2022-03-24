1cac0b0c9beee0b88d92cf602bdc70b0190047d2
Georgi Kostadinov <georgi.kostadinov14@abv.bg>
2022-03-24 15:46:41 +0200

    Create Product and List logic created

---
+++ Services/ShopWebApp.Services.Data/ProductsService.cs
@@ -0,0 +1,57 @@
+﻿using ShopWebApp.Data.Common.Repositories;
+using ShopWebApp.Data.Models;
+using ShopWebApp.Services.Mapping;
+using ShopWebApp.Web.ViewModels.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopWebApp.Services.Data
+{
+    public class ProductsService : IProductsService
+    {
+        private readonly IDeletableEntityRepository<Product> productsRepository;
+
+        public ProductsService(IDeletableEntityRepository<Product> productsRepository)
+        {
+            this.productsRepository = productsRepository;
+        }
+
+        public async Task CreateProduct(InputProductModel inputProductModel)
+        {
+            Product product = this.productsRepository.All().FirstOrDefault(p => p.Name == inputProductModel.Name);
+
+            if (product == null)
+            {
+                product = new Product()
+                {
+                   Name = inputProductModel.Name,
+                   Description = inputProductModel.Description,
+                   ImageURL = inputProductModel.ImageURL,
+                   Price = inputProductModel.Price,
+                   Stock = inputProductModel.Stock,
+                };
+            }
+
+            await this.productsRepository.AddAsync(product);
+            await this.productsRepository.SaveChangesAsync();
+        }
+
+        public Task DeleteProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<ProductViewModel> GetAll()
+        {
+            return this.productsRepository.All().To<ProductViewModel>().Where(p => p.Stock > 0).ToList();
+        }
+
+        public Task Update(int productId, InputProductModel inputProductModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
